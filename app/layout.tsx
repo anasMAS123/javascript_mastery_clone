4602456af18ce7eca7f467fc020a3b2a7091ab0e
Anas Al Saieed <alsaieedanas@gmail.com>
2024-11-25 20:55:36 +0200

    final changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "JS mastery",
   description: "JS mastery clone as a training for using next.js 14 with TS ",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
